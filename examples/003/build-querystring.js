4fe55e4216a97fb43a2ae4aab43886f11c36d94a
Eugene Khristo <marteau9292@gmail.com>
2019-07-07 17:11:07 +0300

    Qury string is build and parsed

--- examples/003/build-querystring.js
+++ examples/003/build-querystring.js
@@ -1,1 +1,10 @@
-const querystring = require('querystring');
+const queryString = require('querystring');
+
+const hostApi = 'https://serjtankian.com/api?';
+const rawQueryString = {
+  username: 'eugene khristo',
+  alias: `idon'tknow`
+};
+
+const urlApi = `${hostApi}${encodeURI(queryString.stringify(rawQueryString))}`;
+process.stdout.write(urlApi);
--- examples/003/parse-querystring.js
+++ examples/003/parse-querystring.js
@@ -1,1 +1,6 @@
 const querystring = require('querystring');
+
+const url = `https://serjtankian.com/api?username=eugene%2520khristo&alias=idon'tknow`;
+const parsedQueryString = querystring.parse(decodeURI(url.split('?')[1]));
+
+console.log(parsedQueryString);
