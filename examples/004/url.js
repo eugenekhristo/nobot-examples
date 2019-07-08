41b01d7af22f0b942dedd1968117eb428b2cf6be
Eugene Khristo <marteau9292@gmail.com>
2019-07-08 08:59:53 +0300

    Url and os model

--- examples/004/url.js
+++ examples/004/url.js
@@ -1,1 +1,9 @@
 const url = require('url');
+const queryString = require('querystring');
+// 'https://serjtankian.com/api?username=eugene%2520khristo&alias=idontknow'
+const [URL] = process.argv.slice(2);
+const parsedUrl = url.parse(URL);
+const parsedQuery = queryString.parse(decodeURI(parsedUrl.query));
+
+console.log(parsedUrl);
+console.log(parsedQuery);
--- examples/005/os.js
+++ examples/005/os.js
@@ -1,1 +1,5 @@
 const os = require('os');
+
+console.log(os.homedir());
+console.log(os.platform());
+console.log(`You have ${os.cpus().length} of ${os.cpus()[0].model}`);
